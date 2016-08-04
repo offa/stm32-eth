9355cf487e8ad1f5ee3379fad338807d206ecf50
offa <bm-dev@yandex.com>
2016-08-04 17:13:59 +0200

    Variable name fixed.

--- cmake/Cpp14.cmake
+++ cmake/Cpp14.cmake
@@ -6,7 +6,7 @@ CHECK_CXX_COMPILER_FLAG("-std=c++1y" CPP_STD_1y)
 
 if( CPP_STD_14 )
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14")
-elseif( CPP_STD_0x )
+elseif( CPP_STD_1y )
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++1y")
 else()
     message(WARNING "C++14 not supported by ${CMAKE_CXX_COMPILER}")
