4a1a5b7ba901b95ab75fe9706c79edfa1d14fa27
offa <bm-dev@yandex.com>
2016-08-31 09:09:55 +0200

    Processor added to Arm Toolchain.

--- arm-embedded-toolchain.cmake
+++ arm-embedded-toolchain.cmake
@@ -1,6 +1,7 @@
 include(CMakeForceCompiler)
 
 set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
 
 CMAKE_FORCE_C_COMPILER(arm-none-eabi-gcc GNU)
 CMAKE_FORCE_CXX_COMPILER(arm-none-eabi-g++ GNU)
