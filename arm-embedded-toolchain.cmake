652847bf876079c678425b1d5ab60499eea89712
offa <bm-dev@yandex.com>
2016-09-07 19:21:08 +0200

    Linker Flags added to Arm Toolchain.

--- arm-embedded-toolchain.cmake
+++ arm-embedded-toolchain.cmake
@@ -15,6 +15,13 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 set(FLAGS_COMMON "-mcpu=cortex-m4 -mthumb -mfloat-abi=soft -ffunction-sections -fdata-sections -ffreestanding -flto" CACHE INTERNAL "C / C++ common Flags")
 set(FLAGS_CXX "-fno-exceptions -fno-rtti -fno-use-cxa-atexit -fno-threadsafe-statics" CACHE INTERNAL "C++ only Flags")
 
-set(CMAKE_C_FLAGS "${FLAGS_COMMON} ${FLAGS_C}" CACHE INTERNAL "C Compiler Flags")
+set(CMAKE_C_FLAGS "${FLAGS_COMMON} ${FLAGS_C}" CACHE STRING "C Compiler Flags")
 set(CMAKE_CXX_FLAGS "${FLAGS_COMMON} ${FLAGS_CXX}" CACHE STRING "C++ Compiler Flags")
 
+
+set(FLAGS_LINKER "-flto -nostartfiles -Xlinker --gc-sections --specs=nano.specs" CACHE INTERNAL "Linker flags")
+
+set(CMAKE_EXE_LINKER_FLAGS "${FLAGS_LINKER}" CACHE STRING "Linker flags")
+set(CMAKE_MODULE_LINKER_FLAGS "${FLAGS_LINKER}" CACHE STRING "Module Linker Flags")
+set(CMAKE_SHARED_LINKER_FLAGS "${FLAGS_LINKER}" CACHE STRING "Shared Library Linker Flags")
+set(CMAKE_STATIC_LINKER_FLAGS "${FLAGS_LINKER}" CACHE STRING "Static Library Linker Flags")
