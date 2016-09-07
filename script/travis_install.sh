fb5c4a8dd89e74104b2d19961e2e334f71c88bb1
offa <bm-dev@yandex.com>
2016-09-07 16:27:04 +0200

    Fixed Clang CI failures by undefined references to stl strings. CppUTest is now compiled using '-stdlib=libc++' on clang.

--- script/travis_install.sh
+++ script/travis_install.sh
@@ -9,14 +9,20 @@ if [ "$CXX" == "arm-none-eabi-g++" ]; then
     sudo apt-get install -y gcc-arm-embedded
 else
     ## Install CppUTest
-    export CPPUTEST_VERSION=3.8
-    export CPPUTEST=cpputest-${CPPUTEST_VERSION}
+    CPPUTEST_VERSION=3.8
+    CPPUTEST=cpputest-${CPPUTEST_VERSION}
+
+    BUILD_FLAGS="-DC++11=ON -DTESTS=OFF"
+
+    if [[ "$CXX" == clang* ]]; then
+        BUILD_FLAGS="$BUILD_FLAGS -DCMAKE_CXX_FLAGS=-stdlib=libc++" 
+    fi
 
     wget https://github.com/cpputest/cpputest/releases/download/v${CPPUTEST_VERSION}/${CPPUTEST}.tar.gz
     tar -xzvf *.tar.gz
     cd ${CPPUTEST}
     mkdir _build && cd _build
-    cmake -DC++11=ON -DTESTS=OFF ..
+    cmake $BUILD_FLAGS ..
     make -j4 && sudo make install
     cd ../..
 fi
