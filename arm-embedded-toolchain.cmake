01b23a156d8a122749a71a874ec8015204a5c02e
offa <bm-dev@yandex.com>
2016-09-07 13:14:50 +0200

    Due CI build failures on Clang, the LTO option has been removed; it's added to the toolchain file directly instead.

--- arm-embedded-toolchain.cmake
+++ arm-embedded-toolchain.cmake
@@ -12,7 +12,7 @@ set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
 
-set(FLAGS_COMMON "-mcpu=cortex-m4 -mthumb -mfloat-abi=soft -ffunction-sections -fdata-sections -ffreestanding" CACHE INTERNAL "C / C++ common Flags")
+set(FLAGS_COMMON "-mcpu=cortex-m4 -mthumb -mfloat-abi=soft -ffunction-sections -fdata-sections -ffreestanding -flto" CACHE INTERNAL "C / C++ common Flags")
 set(FLAGS_CXX "-fno-exceptions -fno-rtti -fno-use-cxa-atexit -fno-threadsafe-statics" CACHE INTERNAL "C++ only Flags")
 
 set(CMAKE_C_FLAGS "${FLAGS_COMMON} ${FLAGS_C}" CACHE INTERNAL "C Compiler Flags")
