dc4814134e8343b613cfcdf0f8f997edfeb89c4c
offa <bm-dev@yandex.com>
2016-09-08 15:04:46 +0200

    Arm Toolchain corrections.

--- arm-embedded-toolchain.cmake
+++ arm-embedded-toolchain.cmake
@@ -12,16 +12,17 @@ set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
 
-set(FLAGS_COMMON "-mcpu=cortex-m4 -mthumb -mfloat-abi=soft -ffunction-sections -fdata-sections -ffreestanding -flto" CACHE INTERNAL "C / C++ common Flags")
+set(FLAGS_COMMON "-mcpu=cortex-m4 -mthumb -mfloat-abi=soft -ffunction-sections -fdata-sections -ffreestanding" CACHE INTERNAL "C / C++ common Flags")
 set(FLAGS_CXX "-fno-exceptions -fno-rtti -fno-use-cxa-atexit -fno-threadsafe-statics" CACHE INTERNAL "C++ only Flags")
 
 set(CMAKE_C_FLAGS "${FLAGS_COMMON} ${FLAGS_C}" CACHE STRING "C Compiler Flags")
 set(CMAKE_CXX_FLAGS "${FLAGS_COMMON} ${FLAGS_CXX}" CACHE STRING "C++ Compiler Flags")
 
 
-set(FLAGS_LINKER "-flto -nostartfiles -Xlinker --gc-sections --specs=nano.specs -T mem.ld -T libs.ld -T sections.ld -L${PROJECT_SOURCE_DIR}" CACHE INTERNAL "Linker flags")
+
+set(FLAGS_LINKER "${FLAGS_COMMON} -nostartfiles -Xlinker --gc-sections --specs=nano.specs -T mem.ld -T libs.ld -T sections.ld -L${PROJECT_SOURCE_DIR}/ldscripts" CACHE INTERNAL "Linker flags")
 
 set(CMAKE_EXE_LINKER_FLAGS "${FLAGS_LINKER}" CACHE STRING "Linker flags")
-set(CMAKE_MODULE_LINKER_FLAGS "${FLAGS_LINKER}" CACHE STRING "Module Linker Flags")
-set(CMAKE_SHARED_LINKER_FLAGS "${FLAGS_LINKER}" CACHE STRING "Shared Library Linker Flags")
-set(CMAKE_STATIC_LINKER_FLAGS "${FLAGS_LINKER}" CACHE STRING "Static Library Linker Flags")
+## set(CMAKE_MODULE_LINKER_FLAGS "${FLAGS_LINKER}" CACHE STRING "Module Linker Flags")
+## set(CMAKE_SHARED_LINKER_FLAGS "${FLAGS_LINKER}" CACHE STRING "Shared Library Linker Flags")
+## set(CMAKE_STATIC_LINKER_FLAGS "${FLAGS_LINKER}" CACHE STRING "Static Library Linker Flags")
