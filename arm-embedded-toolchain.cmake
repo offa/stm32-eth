20ad3e8c0721bdc4eeb635748886da323f177239
offa <bm-dev@yandex.com>
2016-09-09 22:55:45 +0200

    Toolchain cleanup.

--- arm-embedded-toolchain.cmake
+++ arm-embedded-toolchain.cmake
@@ -14,12 +14,8 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
 set(FLAGS_COMMON "-mcpu=cortex-m4 -mthumb -mfloat-abi=soft -ffunction-sections -fdata-sections -ffreestanding" CACHE INTERNAL "C / C++ common Flags")
 set(FLAGS_CXX "-fno-exceptions -fno-rtti -fno-use-cxa-atexit -fno-threadsafe-statics" CACHE INTERNAL "C++ only Flags")
+set(FLAGS_LINKER "${FLAGS_COMMON} -nostartfiles -Xlinker --gc-sections --specs=nano.specs -T mem.ld -T libs.ld -T sections.ld -L${PROJECT_SOURCE_DIR}/ldscripts" CACHE INTERNAL "Linker flags")
 
 set(CMAKE_C_FLAGS "${FLAGS_COMMON} ${FLAGS_C}" CACHE STRING "C Compiler Flags")
 set(CMAKE_CXX_FLAGS "${FLAGS_COMMON} ${FLAGS_CXX}" CACHE STRING "C++ Compiler Flags")
-
-
-
-set(FLAGS_LINKER "${FLAGS_COMMON} -nostartfiles -Xlinker --gc-sections --specs=nano.specs -T mem.ld -T libs.ld -T sections.ld -L${PROJECT_SOURCE_DIR}/ldscripts" CACHE INTERNAL "Linker flags")
-
 set(CMAKE_EXE_LINKER_FLAGS "${FLAGS_LINKER}" CACHE STRING "Linker flags")
