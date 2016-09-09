489995d64de5da3c5c1fa1f7e9924078dc53c866
offa <bm-dev@yandex.com>
2016-09-09 20:04:15 +0200

    Output library sizes on ARM CI builds.

--- script/travis_build.sh
+++ script/travis_build.sh
@@ -7,6 +7,8 @@ mkdir build && cd build
 if [ "$CXX" == "arm-none-eabi-g++" ]; then
     cmake -DUNITTEST=OFF -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_TOOLCHAIN_FILE=../arm-embedded-toolchain.cmake ..
     make
+
+    make eth-stm32.size
 else
     cmake -DUNITTEST_VERBOSE=ON ..
     make
