d74f16af4cc5c0c27c9a09b1ecf9a8d7b022ab21
offa <bm-dev@yandex.com>
2016-09-09 22:53:47 +0200

    Toolchain cleanup.

--- arm-embedded-toolchain.cmake
+++ arm-embedded-toolchain.cmake
@@ -23,6 +23,3 @@ set(CMAKE_CXX_FLAGS "${FLAGS_COMMON} ${FLAGS_CXX}" CACHE STRING "C++ Compiler Fl
 set(FLAGS_LINKER "${FLAGS_COMMON} -nostartfiles -Xlinker --gc-sections --specs=nano.specs -T mem.ld -T libs.ld -T sections.ld -L${PROJECT_SOURCE_DIR}/ldscripts" CACHE INTERNAL "Linker flags")
 
 set(CMAKE_EXE_LINKER_FLAGS "${FLAGS_LINKER}" CACHE STRING "Linker flags")
-## set(CMAKE_MODULE_LINKER_FLAGS "${FLAGS_LINKER}" CACHE STRING "Module Linker Flags")
-## set(CMAKE_SHARED_LINKER_FLAGS "${FLAGS_LINKER}" CACHE STRING "Shared Library Linker Flags")
-## set(CMAKE_STATIC_LINKER_FLAGS "${FLAGS_LINKER}" CACHE STRING "Static Library Linker Flags")
