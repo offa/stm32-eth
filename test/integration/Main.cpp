ffa07d8ec039003b51ff24fbec2feefed1c8ae37
offa <bm-dev@yandex.com>
2016-09-26 20:19:34 +0200

    Missing license headers added.

--- test/integration/Main.cpp
+++ test/integration/Main.cpp
@@ -1,3 +1,22 @@
+/*
+ * Stm32 Eth - Ethernet connectivity for Stm32
+ * Copyright (C) 2016  offa
+ *
+ * This file is part of Stm32 Eth.
+ *
+ * Stm32 Eth is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Stm32 Eth is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Stm32 Eth.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <stm32f4xx_hal.h>
 #include <diag/Trace.h>
