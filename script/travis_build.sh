0ab242b1f4655553206ad3bb2a4f094e0777be68
offa <bm-dev@yandex.com>
2016-10-13 19:42:02 +0200

    Cleanup.

--- script/travis_build.sh
+++ script/travis_build.sh
@@ -8,7 +8,11 @@ export GSL_INCLUDE_DIR=dependencies/gsl
 mkdir build && cd build
 
 if [ "$CXX" == "arm-none-eabi-g++" ]; then
-    cmake -DUNITTEST=OFF -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DINTEGRATION_TEST=ON -DCMAKE_TOOLCHAIN_FILE=../arm-embedded-toolchain.cmake ..
+    cmake -DUNITTEST=OFF \
+            -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
+            -DINTEGRATION_TEST=ON \
+            -DCMAKE_TOOLCHAIN_FILE=../arm-embedded-toolchain.cmake \
+            ..
     make
 
     make eth-stm32.size
