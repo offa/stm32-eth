37230e572d233910c73f13d4b1bc2703ffc5c240
offa <bm-dev@yandex.com>
2016-10-13 12:39:10 +0200

    LTO fully supported. If enabled by it's option, cmake will add compiler / linker flags and update CMAKE_AR to the version with LTO plugin. This is necessary since CMake doesn't support LTO out-of-the box yet.

--- cmake/Compiler.cmake
+++ cmake/Compiler.cmake
@@ -55,6 +55,17 @@ endmacro()
 if( LTO )
     add_common_flag(-flto)
     add_linker_flag(-flto)
+
+
+    find_program(CMAKE_AR_LTO NAMES "${CMAKE_C_COMPILER}-ar" DOC "CMAKE_AR with LTO Plugin")
+
+    set(CMAKE_AR "${CMAKE_AR_LTO}")
+    set(CMAKE_C_ARCHIVE_CREATE "<CMAKE_AR> qcs <TARGET> <LINK_FLAGS> <OBJECTS>")
+    set(CMAKE_C_ARCHIVE_FINISH true)
+    set(CMAKE_CXX_ARCHIVE_CREATE "<CMAKE_AR> qcs <TARGET> <LINK_FLAGS> <OBJECTS>")
+    set(CMAKE_CXX_ARCHIVE_FINISH true)
+
+    mark_as_advanced(CMAKE_AR_LTO)
 endif()
 
 
