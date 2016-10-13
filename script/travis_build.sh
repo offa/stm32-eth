13b6632903581b588b2ebd0b29cc075da6dc2d3e
offa <bm-dev@yandex.com>
2016-10-13 16:52:18 +0200

    Print size only if LTO is not enabled.

--- script/travis_build.sh
+++ script/travis_build.sh
@@ -2,16 +2,11 @@
 
 set -ex
 
-echo "LTO: ${LTO_ENABLED}"
-
 if [ -v LTO_ENABLED ]; then
     LTO_ENABLED=ON
-    echo "--> ON"
 else
     LTO_ENABLED=OFF
-    echo "--> OFF"
 fi
-echo "LTO: ${LTO_ENABLED}"
 
 
 mkdir build && cd build
@@ -20,7 +15,9 @@ if [ "$CXX" == "arm-none-eabi-g++" ]; then
     cmake -DUNITTEST=OFF -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DINTEGRATION_TEST=ON -DLTO=${LTO_ENABLED} -DCMAKE_TOOLCHAIN_FILE=../arm-embedded-toolchain.cmake ..
     make
 
-    make eth-stm32.size
+    if [ "${LTO_ENABLED}" != "ON" ]; then
+        make eth-stm32.size
+    fi
 else
     cmake -DUNITTEST_VERBOSE=ON ..
     make
