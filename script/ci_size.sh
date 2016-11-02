5b42fa9640371d6224e2f2fbbfe02a73a30b4878
offa <bm-dev@yandex.com>
2016-11-02 13:17:36 +0100

    CI build steps moved to script.

---
+++ script/ci_size.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -ex
+
+NAME=../size-lib.csv
+NAME_IT=../size-it.csv
+
+CSV_HEADER="unused,text,data,bss,total,hex,name"
+
+cd build
+
+echo "${CSV_HEADER}" > ${NAME}
+make eth-stm32.size | grep -i totals >> ${NAME}
+sed -i 's/[[:space:]]\+/,/g' ${NAME}
+
+echo "${CSV_HEADER}" > ${NAME_IT}
+make eth-stm32-it.size | grep -i totals >> ../${NAME_IT}
+sed -i 's/[[:space:]]\+/,/g' ${NAME_IT}
+
+cd -
