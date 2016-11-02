d08464f224324c119c82e3f15b7acef09500764b
offa <bm-dev@yandex.com>
2016-11-02 13:08:54 +0100

    CI build steps moved to script.

--- script/ci_analyze.sh
+++ script/ci_analyze.sh
@@ -4,6 +4,22 @@ set -ex
 
 BUILD_DIR=build
 
+
+mkdir ${BUILD_DIR} && cd ${BUILD_DIR}
+
+cmake -DUNITTEST_JUNIT=ON -DCOVERAGE=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+make
+
+## Coverage 
+make coverage-clean
+make unittest
+make coverage
+
+python ${LCOV_COBERTURA}/lcov_cobertura.py coverage/stm32-eth.info --base-dir ../src --output coverage/coverage.xml --demangle
+cd -
+
+
+## CppCheck
 cppcheck -v --error-exitcode=1 \
             --check-config \
             --check-library \
@@ -14,5 +30,6 @@ cppcheck -v --error-exitcode=1 \
             --suppress=missingInclude \
             -i system/src \
             --project="${BUILD_DIR}/compile_commands.json" \
-            src include 2> cppcheck-result.xml
+            src include \
+            2> cppcheck-result.xml
 
