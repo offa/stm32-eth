878a399c943fcd2b688219f40ca6ca3de70bc000
offa <bm-dev@yandex.com>
2017-02-02 14:36:49 +0100

    Checking for supported C++ standard improved.

--- cmake/CppStd.cmake
+++ cmake/CppStd.cmake
@@ -1,19 +1,21 @@
+set(CPP_STD         14)
+set(CPP_STD_PRE     1y)
 
 if( NOT CMAKE_CROSSCOMPILING )
     include(CheckCXXCompilerFlag)
 
-    CHECK_CXX_COMPILER_FLAG("-std=c++14" CPP_STD_14)
-    CHECK_CXX_COMPILER_FLAG("-std=c++1y" CPP_STD_1y)
+    CHECK_CXX_COMPILER_FLAG("-std=c++${CPP_STD}" CPP_STD_SUPPORTED)
+    CHECK_CXX_COMPILER_FLAG("-std=c++${CPP_STD_PRE}" CPP_STD_PRE_SUPPORTED)
 
-    if( CPP_STD_14 )
-        add_cxx_flag(-std=c++14)
-    elseif( CPP_STD_0x )
-        add_cxx_flag(-std=c++1y)
+    if( CPP_STD_SUPPORTED )
+        add_cxx_flag(-std=c++${CPP_STD})
+    elseif( CPP_STD_PRE_SUPPORTED )
+        add_cxx_flag(-std=c++${CPP_STD_PRE})
     else()
-        message(WARNING "C++14 not supported by ${CMAKE_CXX_COMPILER}")
+        message(WARNING "C++${CPP_STD} not supported by ${CMAKE_CXX_COMPILER}")
     endif()
 
 else()
-    add_cxx_flag(-std=c++14)
+    add_cxx_flag(-std=c++${CPP_STD})
 endif()
 
