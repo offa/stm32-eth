86758a3f1268e2fa89059580fb00580df54ae785
offa <bm-dev@yandex.com>
2017-04-13 12:58:49 +0200

    CMake code refactored; LTO option moved out from the compiler module.

--- cmake/Compiler.cmake
+++ cmake/Compiler.cmake
@@ -1,5 +1,4 @@
 
-
 macro(add_common_flag)
     foreach(_flag ${ARGN})
         set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${_flag}")
@@ -50,33 +49,6 @@ macro(add_size_target _target)
                                         )
     endif()
 endmacro()
-
-
-if( LTO )
-    add_common_flag(-flto)
-    add_linker_flag(-flto)
-
-
-    find_program(CMAKE_AR_LTO ${CMAKE_C_COMPILER}-ar
-                                NAMES gcc-ar
-                                DOC "CMAKE_AR with LTO Plugin"
-                                )
-
-    if( CMAKE_AR_LTO )
-        set(CMAKE_AR "${CMAKE_AR_LTO}")
-        set(CMAKE_C_ARCHIVE_CREATE "<CMAKE_AR> qcs <TARGET> <LINK_FLAGS> <OBJECTS>")
-        set(CMAKE_C_ARCHIVE_FINISH true)
-        set(CMAKE_CXX_ARCHIVE_CREATE "<CMAKE_AR> qcs <TARGET> <LINK_FLAGS> <OBJECTS>")
-        set(CMAKE_CXX_ARCHIVE_FINISH true)
-
-        mark_as_advanced(CMAKE_AR_LTO)
-    else()
-        message(WARNING "CMAKE_AR_LTO not found")
-    endif()
-
-
-
-endif()
 
 
 
