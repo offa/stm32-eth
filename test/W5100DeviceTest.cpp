cd41dcadbc3674d839b0cf21ebc0e64882ae833b
offa <bm-dev@yandex.com>
2017-07-24 19:01:04 +0200

    Using structured bindings.

--- test/W5100DeviceTest.cpp
+++ test/W5100DeviceTest.cpp
@@ -443,15 +443,17 @@ TEST(W5100DeviceTest, configureNetConfiguration)
         {{0x00, 0x08, 0xdc, 0x01, 0x02, 0x03}}
     };
 
+    const auto[ip, netmask, gateway, mac] = config;
+
     constexpr std::uint16_t addrIp = 0x000f;
     constexpr std::uint16_t addrNetmask = 0x0005;
     constexpr std::uint16_t addrGateway = 0x0001;
     constexpr std::uint16_t addrMac = 0x0009;
 
-    expectWrite(addrIp, std::get<0>(config));
-    expectWrite(addrNetmask, std::get<1>(config));
-    expectWrite(addrGateway, std::get<2>(config));
-    expectWrite(addrMac, std::get<3>(config));
+    expectWrite(addrIp, ip);
+    expectWrite(addrNetmask, netmask);
+    expectWrite(addrGateway, gateway);
+    expectWrite(addrMac, mac);
 
     setupDevice(*device, config);
 }
