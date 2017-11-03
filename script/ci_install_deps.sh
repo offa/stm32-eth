107bc2ee75fd0dead225b466097417d37c5f59f6
offa <bm-dev@yandex.com>
2017-11-03 18:16:18 +0100

    Revert "Missing sudo added for the installation."
    This reverts commit bf73bc4d5e7da9cba667633451c1648683266b91.

--- script/ci_install_deps.sh
+++ script/ci_install_deps.sh
@@ -16,7 +16,7 @@ fi
 cd gsl
 mkdir -p build-${CC} && cd build-${CC}
 cmake -DGSL_TEST=OFF ..
-sudo make install
+make install
 
 
 
@@ -43,8 +43,7 @@ then
 
     mkdir -p _build-${CC} && cd _build-${CC}
     cmake ${BUILD_FLAGS} ..
-    make -j4
-    sudo make install
+    make -j4 && sudo make install
 fi
 
 cd ${DEPENDENCY_DIR}
