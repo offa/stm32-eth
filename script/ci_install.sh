f11dd420939f7d31eeb22eccac610e6039e3603c
offa <bm-dev@yandex.com>
2017-11-18 20:07:33 +0100

    CMake updated to v3.9.4 on CI (#67).

--- script/ci_install.sh
+++ script/ci_install.sh
@@ -12,7 +12,7 @@ CMAKE_INSTALLER=install-cmake.sh
 
 if [[ ! -f ${CMAKE_INSTALLER} ]]
 then
-    curl -sSL https://cmake.org/files/v3.8/cmake-3.8.2-Linux-x86_64.sh -o ${CMAKE_INSTALLER}
+    curl -sSL https://cmake.org/files/v3.9/cmake-3.9.4-Linux-x86_64.sh -o ${CMAKE_INSTALLER}
     chmod +x ${CMAKE_INSTALLER}
 fi
 
