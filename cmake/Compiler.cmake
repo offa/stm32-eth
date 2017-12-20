9e8d5db29ff0edb8bb5c549d48d144c3a679f0a6
offa <bm-dev@yandex.com>
2017-12-20 20:04:09 +0100

    Variable names fixed.

--- cmake/Compiler.cmake
+++ cmake/Compiler.cmake
@@ -1,40 +1,40 @@
 macro(add_c_flag)
-    foreach(_flag ${ARGN})
-        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${_flag}")
+    foreach(flag ${ARGN})
+        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${flag}")
     endforeach()
 endmacro()
 
 macro(add_cxx_flag)
-    foreach(_flag ${ARGN})
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${_flag}")
+    foreach(flag ${ARGN})
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${flag}")
     endforeach()
 endmacro()
 
 macro(add_compiler_flag)
-    foreach(_flag ${ARGN})
-        add_c_flag(${_flag})
-        add_cxx_flag(${_flag})
+    foreach(flag ${ARGN})
+        add_c_flag(${flag})
+        add_cxx_flag(${flag})
     endforeach()
 endmacro()
 
 macro(add_linker_flag)
-    foreach(_flag ${ARGN})
-        set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${_flag}")
+    foreach(flag ${ARGN})
+        set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${flag}")
     endforeach()
 endmacro()
 
 
-function(add_hex_target _target)
-    add_custom_target(${_target}.hex DEPENDS ${_target}
-                                    COMMAND ${CMAKE_OBJCOPY} -Oihex $<TARGET_FILE:${_target}> ${_target}.hex
+function(add_hex_target targetname)
+    add_custom_target(${targetname}.hex DEPENDS ${targetname}
+                                    COMMAND ${CMAKE_OBJCOPY} -Oihex $<TARGET_FILE:${targetname}> ${targetname}.hex
                                     VERBATIM
                                     )
 endfunction()
 
 
-function(add_bin_target _target)
-    add_custom_target(${_target}.bin DEPENDS ${_target}
-                                    COMMAND ${CMAKE_OBJCOPY} -Obinary $<TARGET_FILE:${_target}> ${_target}.bin
+function(add_bin_target targetname)
+    add_custom_target(${targetname}.bin DEPENDS ${targetname}
+                                    COMMAND ${CMAKE_OBJCOPY} -Obinary $<TARGET_FILE:${targetname}> ${targetname}.bin
                                     VERBATIM
                                     )
 endfunction()
@@ -42,12 +42,12 @@ endfunction()
 
 find_program(SIZE_EXE size)
 
-function(add_size_target _target)
+function(add_size_target targetname)
     if( NOT SIZE_EXE )
         message(WARNING "'size' not found!")
     else()
-        add_custom_target(${_target}.size DEPENDS ${_target}
-                                        COMMAND ${SIZE_EXE} -t --format=berkeley $<TARGET_FILE:${_target}>
+        add_custom_target(${targetname}.size DEPENDS ${targetname}
+                                        COMMAND ${SIZE_EXE} -t --format=berkeley $<TARGET_FILE:${targetname}>
                                         VERBATIM
                                         )
     endif()
