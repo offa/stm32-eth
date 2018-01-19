99e094daa44a6df035e2705305df439f5ee1f72c
offa <bm-dev@yandex.com>
2018-01-19 11:38:17 +0100

    Cleanup of the previous functions for compiler flag handling.

--- cmake/Compiler.cmake
+++ cmake/Compiler.cmake
@@ -1,28 +1,3 @@
-macro(add_c_flag)
-    foreach(flag ${ARGN})
-        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${flag}")
-    endforeach()
-endmacro()
-
-macro(add_cxx_flag)
-    foreach(flag ${ARGN})
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${flag}")
-    endforeach()
-endmacro()
-
-macro(add_compiler_flag)
-    foreach(flag ${ARGN})
-        add_c_flag(${flag})
-        add_cxx_flag(${flag})
-    endforeach()
-endmacro()
-
-macro(add_linker_flag)
-    foreach(flag ${ARGN})
-        set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${flag}")
-    endforeach()
-endmacro()
-
 
 function(add_hex_target targetname)
     add_custom_target(${targetname}.hex DEPENDS ${targetname}
