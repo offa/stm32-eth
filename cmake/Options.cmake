89892ae3175e4212f67a09533431fafc4e8ad423
offa <bm-dev@yandex.com>
2018-04-16 21:53:11 +0200

    CMake Test option naming unified (fixes #103).

--- cmake/Options.cmake
+++ cmake/Options.cmake
@@ -26,8 +26,8 @@ print_option(LTO "Enable LTO")
 option(BUILD_SYSTEM_DRIVER "Build system files" ${CMAKE_CROSSCOMPILING})
 print_option(BUILD_SYSTEM_DRIVER "Build system driver")
 
-option(INTEGRATION_TEST "Build Integration Tests" OFF)
-print_option(INTEGRATION_TEST "Build Integration Tests")
+option(INTEGRATIONTEST "Build Integration Tests" OFF)
+print_option(INTEGRATIONTEST "Build Integration Tests")
 
 option(GSL_CONTRACT_UNENFORCED "GSL contract violations unenforced" ${CMAKE_CROSSCOMPILING})
 print_option(GSL_CONTRACT_UNENFORCED "Unenforced GSL contracts")
