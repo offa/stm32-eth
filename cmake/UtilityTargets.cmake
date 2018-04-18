19254797c28c3cb365f226aef3cb20768d5eb259
offa <bm-dev@yandex.com>
2018-04-18 19:31:44 +0200

    CMake code improvements.

--- cmake/UtilityTargets.cmake
+++ cmake/UtilityTargets.cmake
@@ -1,29 +1,32 @@
 function(add_hex_target targetname)
-    add_custom_target(${targetname}.hex DEPENDS ${targetname}
-                                    COMMAND ${CMAKE_OBJCOPY} -Oihex $<TARGET_FILE:${targetname}> ${targetname}.hex
-                                    VERBATIM
-                                    )
+    add_custom_target(${targetname}.hex
+                        DEPENDS ${targetname}
+                        COMMAND ${CMAKE_OBJCOPY} -Oihex $<TARGET_FILE:${targetname}> ${targetname}.hex
+                        VERBATIM
+                        )
 endfunction()
 
 
 function(add_bin_target targetname)
-    add_custom_target(${targetname}.bin DEPENDS ${targetname}
-                                    COMMAND ${CMAKE_OBJCOPY} -Obinary $<TARGET_FILE:${targetname}> ${targetname}.bin
-                                    VERBATIM
-                                    )
+    add_custom_target(${targetname}.bin
+                        DEPENDS ${targetname}
+                        COMMAND ${CMAKE_OBJCOPY} -Obinary $<TARGET_FILE:${targetname}> ${targetname}.bin
+                        VERBATIM
+                        )
 endfunction()
 
 
-find_program(SIZE_EXE size)
+find_program(SIZE_BIN size)
 
 function(add_size_target targetname)
-    if( NOT SIZE_EXE )
+    if( NOT SIZE_BIN )
         message(WARNING "'size' not found!")
     else()
-        add_custom_target(${targetname}.size DEPENDS ${targetname}
-                                        COMMAND ${SIZE_EXE} -t --format=berkeley $<TARGET_FILE:${targetname}>
-                                        VERBATIM
-                                        )
+        add_custom_target(${targetname}.size
+                            DEPENDS ${targetname}
+                            COMMAND ${SIZE_BIN} -t --format=berkeley $<TARGET_FILE:${targetname}>
+                            VERBATIM
+                            )
     endif()
 endfunction()
 
