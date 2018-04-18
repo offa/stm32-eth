c34000ef565c17205fdb483c85d7dd9941887846
offa <bm-dev@yandex.com>
2018-04-18 19:40:37 +0200

    Function for hex/bin/size targets merged (#106).

--- cmake/UtilityTargets.cmake
+++ cmake/UtilityTargets.cmake
@@ -1,35 +1,38 @@
-function(add_hex_target targetname)
-    add_custom_target(${targetname}.hex
-                        DEPENDS ${targetname}
-                        COMMAND ${CMAKE_OBJCOPY} -Oihex $<TARGET_FILE:${targetname}> ${targetname}.hex
-                        VERBATIM
-                        )
-endfunction()
-
-
-function(add_bin_target targetname)
-    add_custom_target(${targetname}.bin
-                        DEPENDS ${targetname}
-                        COMMAND ${CMAKE_OBJCOPY} -Obinary $<TARGET_FILE:${targetname}> ${targetname}.bin
-                        VERBATIM
-                        )
-endfunction()
-
+include(CMakeParseArguments)
 
 find_program(SIZE_BIN size)
 
-function(add_size_target targetname)
-    if( NOT SIZE_BIN )
-        message(WARNING "'size' not found!")
-    else()
-        add_custom_target(${targetname}.size
+
+function(add_utility_target targetname)
+    set(options BIN_FILE HEX_FILE SIZE)
+    cmake_parse_arguments(TARGET_OPTIONS "${options}" "" "" ${ARGN})
+
+    if( TARGET_OPTIONS_BIN_FILE )
+        add_custom_target(${targetname}.hex
                             DEPENDS ${targetname}
-                            COMMAND ${SIZE_BIN} -t --format=berkeley $<TARGET_FILE:${targetname}>
+                            COMMAND ${CMAKE_OBJCOPY} -Oihex $<TARGET_FILE:${targetname}> ${targetname}.hex
                             VERBATIM
                             )
     endif()
+
+    if( TARGET_OPTIONS_HEX_FILE )
+        add_custom_target(${targetname}.bin
+                            DEPENDS ${targetname}
+                            COMMAND ${CMAKE_OBJCOPY} -Obinary $<TARGET_FILE:${targetname}> ${targetname}.bin
+                            VERBATIM
+                            )
+    endif()
+
+    if( TARGET_OPTIONS_SIZE )
+        if( NOT SIZE_BIN )
+            message(WARNING "'size' not found!")
+        else()
+            add_custom_target(${targetname}.size
+                                DEPENDS ${targetname}
+                                COMMAND ${SIZE_BIN} -t --format=berkeley $<TARGET_FILE:${targetname}>
+                                VERBATIM
+                                )
+        endif()
+    endif()
 endfunction()
-
-
-
 
