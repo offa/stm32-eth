ee9e9f2b1774350a700a7302ad6a16165587a057
offa <bm-dev@yandex.com>
2018-04-23 19:17:47 +0200

    Nosys.specs added to the ARM GCC Linker flags (#110).

--- arm-embedded-toolchain.cmake
+++ arm-embedded-toolchain.cmake
@@ -14,7 +14,7 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 set(FLAGS_COMMON "-mcpu=cortex-m4 -mthumb -mfloat-abi=soft -ffunction-sections -fdata-sections -ffreestanding")
 set(FLAGS_CXX "-fno-unwind-tables -fno-exceptions -fno-rtti")
 set(FLAGS_C "")
-set(FLAGS_LINKER "-nostartfiles -Xlinker --gc-sections --specs=nano.specs -T mem.ld -T libs.ld -T sections.ld -L${PROJECT_SOURCE_DIR}/ldscripts -Wl,-Map,${PROJECT_NAME}.map")
+set(FLAGS_LINKER "-nostartfiles -Xlinker --gc-sections --specs=nano.specs --specs=nosys.specs -T mem.ld -T libs.ld -T sections.ld -L${PROJECT_SOURCE_DIR}/ldscripts -Wl,-Map,${PROJECT_NAME}.map")
 
 set(CMAKE_C_FLAGS_INIT "${FLAGS_COMMON} ${FLAGS_C}" CACHE STRING "C Compiler Flags")
 set(CMAKE_CXX_FLAGS_INIT "${FLAGS_COMMON} ${FLAGS_CXX}" CACHE STRING "C++ Compiler Flags")
