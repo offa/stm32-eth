39190e2f46d16a492fccd20c9c4e05261e23e96c
offa <bm-dev@yandex.com>
2020-12-15 18:59:21 +0100

    CI build of arm-none-eabi-gcc fixed (#160).

--- script/ci_build.sh
+++ script/ci_build.sh
@@ -37,7 +37,7 @@ fi
 mkdir -p build && cd build
 
 
-if [[ "${CXX}" == "arm-none-eabi-g++" ]]
+if [[ "${COMPILER}" == "arm-none-eabi-gcc" ]]
 then
     cmake "${BUILD_ARGS[@]}" \
             -DUNITTEST=OFF \
