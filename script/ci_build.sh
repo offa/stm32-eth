ea2bce1cbf842f426e2cf1bb3f57b25930a014c6
offa <bm-dev@yandex.com>
2022-07-27 20:42:17 +0200

    Use compiler image and inline dependency installation (#168)

--- script/ci_build.sh
+++ script/ci_build.sh
@@ -48,6 +48,14 @@ then
         make stm32-eth-client-it.size
     fi
 else
+    # Install CppUTest
+    git clone --branch=latest-passing-build --depth=1 https://github.com/cpputest/cpputest.git cpputest
+    mkdir -p cpputest/_build
+    cd cpputest/_build
+    cmake -DCMAKE_CXX_STANDARD=20 -DC++11=ON -DTESTS=OFF -DMEMORY_LEAK_DETECTION=OFF ..
+    make -j install
+    cd ../..
+
     cmake "${BUILD_ARGS[@]}" \
             -DUNITTEST_VERBOSE=ON \
             ..
